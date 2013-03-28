ccdaba68b5c2354526d6e516878c7a3891dad8b3
Randy Ridge <randyridge@gmail.com>
2013-03-27 20:26:08 -0400

    Extracted bitmap writer from Program.

---
+++ SmallPathTracer/BitmapWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System;
+
+namespace SmallPathTracer {
+    public static class BitmapWriter {
+        // --- Private Static Methods ---
+        private static double Clamp(double x) {
+            return x < 0 ? 0 : x > 1 ? 1 : x;
+        }
+
+        private static int ToInt(double x) {
+            return (int) (Math.Pow(Clamp(x), 1 / 2.2) * 255 + .5);
+        }
+
+        // --- Public Static Methods ---
+        public static void Write(string fileName, int width, int height, Vector[] data) {
+            var bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            for(var y = 0; y < height; y++) {
+                for(var x = 0; x < width; x++) {
+                    var i = (1 + y - 1) * width + x;
+                    bitmap.SetPixel(x, y, Color.FromArgb(ToInt(data[i].X), ToInt(data[i].Y), ToInt(data[i].Z)));
+                }
+            }
+            bitmap.Save(fileName, ImageFormat.Png);
+        }
+    }
+}
--- SmallPathTracer/Program.cs
+++ SmallPathTracer/Program.cs
@@ -1,35 +1,14 @@
-﻿using System.Drawing.Imaging;
-using System.Drawing;
-using System;
-
-namespace SmallPathTracer {
+﻿namespace SmallPathTracer {
     public static class Program {
-        // --- Private Static Methods ---
-        private static double Clamp(double x) {
-            return x < 0 ? 0 : x > 1 ? 1 : x;
-        }
-
-        private static int ToInt(double x) {
-            return (int) (Math.Pow(Clamp(x), 1 / 2.2) * 255 + .5);
-        }
-
         // --- Public Static Methods ---
         public static void Main(string[] args) {
-            var width = int.Parse(args[0]); // width
-            var height = int.Parse(args[1]); // height
-            var samples = int.Parse(args[2]); // # samples
+            var width = int.Parse(args[0]);
+            var height = int.Parse(args[1]);
+            var samples = int.Parse(args[2]);
             var outputFileName = args[3];
             var renderer = new Renderer(width, height, samples, int.Parse(args[4]));
             var output = renderer.Render();
-
-            var bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            for(var y = 0; y < height; y++) {
-                for(var x = 0; x < width; x++) {
-                    var i = (1 + y - 1) * width + x;
-                    bitmap.SetPixel(x, y, Color.FromArgb(ToInt(output[i].X), ToInt(output[i].Y), ToInt(output[i].Z)));
-                }
-            }
-            bitmap.Save(outputFileName, ImageFormat.Png);
+            BitmapWriter.Write(outputFileName, width, height, output);
         }
     }
 }
